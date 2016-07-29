95a4c6c8038b2dec360a6344a538c12f9f60172a
cici-tan <cici_t45@yahoo.com>
2016-07-28 22:54:25 -0700

    add map

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Made in CHINA' });
+  res.render('index', { title: 'Story of CHINA' });
 });
 
 module.exports = router;
