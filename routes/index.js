d148204177c9fbbbba598f4f5fe8cf0d4da2e1a8
cici-tan <cici_t45@yahoo.com>
2016-07-29 23:46:27 -0700

    add navbar

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Story of CHINA' });
+  res.render('index', { title: 'CHINAsee' });
 });
 
 module.exports = router;
