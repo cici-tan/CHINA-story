1044f2b451faeed5d9129f324e9fdf4ddaa33546
cici-tan <cici_t45@yahoo.com>
2016-08-04 12:54:19 -0700

    popup pic

--- routes/cityView.js
+++ routes/cityView.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next, stateName) {
-    res.render('cityView', {title: stateName});
+router.get('/', function(req, res, next) {
+    res.render('cityView');
 });
 
 module.exports = router;
